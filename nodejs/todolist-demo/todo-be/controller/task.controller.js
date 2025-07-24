6739bb65fab109080d6345ffb9b1e22b095b213f
Hyungjoon Bae <peihengjun1830@gmail.com>
2025-07-24 06:55:45 -0700

    첫번째 과제

--- nodejs/todolist-demo/todo-be/controller/task.controller.js
+++ nodejs/todolist-demo/todo-be/controller/task.controller.js
@@ -22,4 +22,30 @@ taskController.getTask = async (req, res) => {
   }
 };
 
+taskController.updateTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const updateTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        task,
+        isComplete,
+      },
+      { new: true, runValidators: true }
+    );
+    res.status(200).json({ status: "ok", data: updateTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const deleteTask = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ status: "ok", data: deleteTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
 module.exports = taskController;
