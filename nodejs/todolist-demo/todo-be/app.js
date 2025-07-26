d047d764f43f616b35d61e1f559677547a66d9bb
Hyungjoon Bae <peihengjun1830@gmail.com>
2025-07-26 14:46:09 -0700

    login-be-modified

--- nodejs/todolist-demo/todo-be/app.js
+++ nodejs/todolist-demo/todo-be/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = "mongodb://localhost:27017/todo-demo";
+const mongoURI = process.env.MONGODB_URI_PROD;
+
+console.log("Mongo URI:", process.env.MONGODB_URI_PROD);
+
 mongoose
   .connect(mongoURI)
   .then(() => {
@@ -19,6 +23,6 @@ mongoose
     console.log("DB connection fail", err);
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
