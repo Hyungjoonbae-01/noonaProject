7af2b87d79c90830aabab81fdb7425c3edf5c0e0
Hyungjoon Bae <peihengjun1830@gmail.com>
2025-07-30 03:01:26 -0700

    PrivateRoute

--- nodejs/todolist-demo/todo-fe/src/App.js
+++ nodejs/todolist-demo/todo-fe/src/App.js
@@ -4,11 +4,27 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./route/PrivateRoute";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const getUser = () => {
+    try {
+      const token = sessionStorage.getItem("token");
+    } catch (error) {}
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute user={user}>
+            <TodoPage />
+          </PrivateRoute>
+        }
+      />
       <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />
